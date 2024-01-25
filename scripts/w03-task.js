f6e0e70d01b259dfc9965c9dea362489314de0e1
eluwa101 <eluwamonday2017@gmail.com>
2024-01-25 23:04:56 +0100

    added the weeks assignment

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -54,21 +54,41 @@ function calculateTotal()
 {
     const subtotalInput = document.getElementById('subtotal');
     const subtotal = parseFloat(subtotalInput.value) || 0; 
-    const isaMember = document.getElementById('member').Checked;
+    const isaMember = document.getElementById('member').checked;
     const discount = isaMember ? 0.2 : 0;
     const total = subtotal * (1 - discount);
     const totalSpan = document.getElementById('total');
     totalSpan.textContent = `$ ${total.toFixed(2)}`;
 } 
 /* ARRAY METHODS - Functional Programming */
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
 /* Output Source Array */
+let theArray = document.getElementById('array');
+theArray.textContent = numbersArray.join(', ');
 
 /* Output Odds Only Array */
+let oddNumsList = numbersArray.filter(number => number % 2 !== 0);
+let oddNumElement = document.getElementById('odds');
+oddNumElement.textContent = oddNumsList.join(', ')
 
 /* Output Evens Only Array */
+let evenNumList = numbersArray.filter(number => number % 2 === 0);
+let evenNumElement = document.getElementById('evens');
+evenNumElement.textContent = evenNumList.join(', ');
 
 /* Output Sum of Org. Array */
+let numSum = numbersArray.reduce((sum, number) => sum + number, 0);
+let arraySumElement = document.getElementById('sumOfArray');
+arraySumElement.textContent = numSum.toString(); 
 
 /* Output Multiplied by 2 Array */
+let multiple = numbersArray.map(number => number * 2)
+let themultiplyElement = document.getElementById('multiplied');
+themultiplyElement.textContent = multiple.join(', ')
 
 /* Output Sum of Multiplied by 2 Array */
+let doubleTheNums = numbersArray.map(number => number * 2);
+let sumedMultiples = doubleTheNums.reduce((sum, number) => sum + number, 0);
+let sumedMultiplesElement = document.getElementById('sumOfMultiplied');
+sumedMultiplesElement.textContent = sumedMultiples.toString(); 
